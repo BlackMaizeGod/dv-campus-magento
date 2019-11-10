2371c9d470c7c6d8d7039617be082052fbb3e5b2
Artem Lohvynenko <artuom.logv@gmail.com>
2019-11-11 01:14:04 +0200

    Created own theme. Configurated grunt for new theme.Created and pushed database dump to the repository

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,12 @@
+module.exports = {
+    arteml_penumbra_en_US: {
+        area: 'frontend',
+        name: 'Arteml/penumbra',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+};
