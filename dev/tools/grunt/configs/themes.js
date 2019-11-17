3b1b38d9c5034431c1a499e2faec1444b4f78293
Artem Lohvynenko <artuom.logv@gmail.com>
2019-11-17 23:40:51 +0200

    Started to change tabs colors

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -19,6 +19,16 @@
  *
  */
 module.exports = {
+    arteml_penumbra_en_US: {
+        area: 'frontend',
+        name: 'Arteml/penumbra',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     blank: {
         area: 'frontend',
         name: 'Magento/blank',
