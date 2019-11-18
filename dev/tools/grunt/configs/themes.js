3c8ce86aaddf422192b825cbbea527dfe369a4a2
Artem Lohvynenko <arteml@default-value.com>
2019-11-18 16:21:36 +0200

    Fineshed task 9 (changed styles for header, footer , alerts, tabs, and fix styles for menu modile)

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -19,16 +19,6 @@
  *
  */
 module.exports = {
-    arteml_penumbra_en_US: {
-        area: 'frontend',
-        name: 'Arteml/penumbra',
-        locale: 'en_US',
-        files: [
-            'css/styles-m',
-            'css/styles-l'
-        ],
-        dsl: 'less'
-    },
     blank: {
         area: 'frontend',
         name: 'Magento/blank',
