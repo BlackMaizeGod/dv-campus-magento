53e366af23f869eae4e4009763a48988ce96c35d
Artem Lohvynenko <arteml@default-value.com>
2019-11-22 14:02:56 +0200

    improve code style

--- app/design/frontend/Arteml/penumbra/registration.php
+++ app/design/frontend/Arteml/penumbra/registration.php
@@ -1,5 +1,5 @@
 <?php
 
-use \Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Arteml/penumbra', __DIR__);
