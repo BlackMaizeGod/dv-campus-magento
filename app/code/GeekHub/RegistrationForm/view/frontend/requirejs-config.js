45fd2066cc5937cb54403e190273166315812d99
Artem Lohvynenko <arteml@default-value.com>
2019-12-06 17:23:47 +0200

    complete task 5 | GeekHub

--- app/code/GeekHub/RegistrationForm/view/frontend/requirejs-config.js
+++ app/code/GeekHub/RegistrationForm/view/frontend/requirejs-config.js
@@ -4,7 +4,7 @@ var config = {
     },
     shim: {
         additionalRegistrationForm: {
-            deps: ['jquery']
+            deps: ['jquery', 'Magento_Ui/js/modal/modal']
         }
     }
 };
